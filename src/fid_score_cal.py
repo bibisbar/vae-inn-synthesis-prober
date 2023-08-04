54e4c9a7241e746de0055a05cf73478e7e8a68f9
bibisbar <bijinhe113@gmail.com>
2023-08-04 21:46:50 +0200

    add fid calculate in callback

---
+++ src/fid_score_cal.py
@@ -0,0 +1,25 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from pytorch_fid import fid_score
+
+
+real_images_folder = 'path_to_real_images'
+# generated_images_folder = './FID_app3'
+generated_images_folder = 'path_to_generated_images'
+
+def calculate_fid(real_images_folder, generated_images_folder):
+    inception_model = torchvision.models.inception_v3(pretrained=True)
+
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+    ])
+
+    fid_value = fid_score.calculate_fid_given_paths([real_images_folder, generated_images_folder],
+                                                    inception_model,
+                                                    transform=transform)
+    print('FID value:', fid_value)
+    return fid_value
+    
--- src/vae_inn_train.py
+++ src/vae_inn_train.py
@@ -13,70 +13,71 @@ from pytorch_lightning.callbacks import Callback
 from pytorch_lightning.trainer import Trainer
 import matplotlib.pyplot as plt
 import torchvision
+import deeplake
 from diffusers.image_processor import VaeImageProcessor
 
+from dataloaders.dataloader_ffhq import ClassificationDataset
+from fid_score_cal import calculate_fid
+
+
+#initialize dataloader
+ds = deeplake.load("hub://activeloop/ffhq")
+ds.summary()
+transform = Compose(
+    [ToTensor()])
+ffhq_pytorch = ClassificationDataset(ds, transform = transform)
+
+train_loader= DataLoader(ffhq_pytorch, batch_size = 48, num_workers = 2, shuffle = True)
 #add tensorboard logger
 logger = TensorBoardLogger("tb_logs", name="my_model")
 print(torch.cuda.is_available())
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-def normalize(x):
-   return x * 2. - 1.
-def normalize_cifar10(x):
-   return x / 255.
-def make_rgb(x):
-   return torch.cat([x,x,x]) # For single-channel images
-def denormalize(x):
-   return (x + 1.) / 2.
-
-transform = Compose(
-    [ToTensor(), normalize])
-
-train_set = datasets.CIFAR10(root='data', train=True,
-                              download=True, transform=transform)
-print(train_set)
-#val_set = datasets.CIFAR10(root='data', train=False,
-#                              download=True, transform=transform)
-
-# Example usage
-# Assuming you have train_dataset and val_dataset as torch.utils.data.Dataset objects
+#initialize model
 
 model = VaeInnModel()
 pl.seed_everything(42)
-train_loader = DataLoader(train_set, batch_size=model.BATCH_SIZE, shuffle=True)
-#val_loader = DataLoader(val_set, batch_size=model.BATCH_SIZE)
 
-# if DEVICE == 'cpu':
-#   devices = 0
-# else:
-#   devices = 1
 
 class ModelSaveCallback(Callback):
     def on_train_start(self, trainer, pl_module):
         print("Training is starting")
 
     def on_train_epoch_start(self, trainer: Trainer, pl_module: pl.LightningModule) -> None:
-        if trainer.current_epoch % 5 == 0:
-          torch.save(model.state_dict(), "model.pt")
-          BATCH_SIZE = 30
-          latents = model.innmodule.inn.inverse(torch.randn((BATCH_SIZE, 4*4*4)).cuda()).detach()
-          latents = latents.reshape((BATCH_SIZE, 4, 4, 4))
-          imgs = model.encoder.sd_vae.tiled_decode(latents * (1./model.scale_factor)).sample
-          #transform = Grayscale(1) # for single-channel datasets
-          #imgs = transform(imgs)
-          grid_img = torchvision.utils.make_grid(imgs, nrow=5)
-          torchvision.utils.save_image(grid_img, f"/export/home/ra35tiy/vae-inn-synthesis-prober/src/outputs_vae_cifar10/epoch_{trainer.current_epoch}.png")
+      if trainer.current_epoch % 20 == 0:
+         torch.save(model.state_dict(), "model.pt")
+         BATCH_SIZE = 5
+         latents = model.innmodule.inn.inverse(torch.randn((BATCH_SIZE, 4*4*4)).cuda()).detach()
+         latents = latents.reshape((BATCH_SIZE, 4, 4, 4))
+         #imgs = model.encoder.sd_vae.tiled_decode(latents * (1./model.scale_factor)).sample
+         imgs = model.encoder.sd_vae.tiled_decode(latents).sample
+         grid_img = torchvision.utils.make_grid(imgs, nrow=5)
+         torchvision.utils.save_image(grid_img, f"/export/home/ra35tiy/vae-inn-synthesis-prober/results/outputs_ffhq/epoch_{trainer.current_epoch}.png")
 
+         ori_data_path = "/export/home/ra35tiy/vae-inn-synthesis-prober/src/data/ffhq_images"
+         gene_data_path = "/export/home/ra35tiy/vae-inn-synthesis-prober/results/sample_per_epoch"
+         #generate images
+         batchsize_generate = 10000
+         latents = model.innmodule.inn.inverse(torch.randn((batchsize_generate, 4*4*4)).cuda()).detach()
+         latents = latents.reshape((batchsize_generate, 4, 4, 4))
+         #imgs = model.encoder.sd_vae.tiled_decode(latents * (1./model.scale_factor)).sample
+         imgs = model.encoder.sd_vae.tiled_decode(latents).sample
+         for i in range(10000):
+            torchvision.utils.save_image(imgs[i], gene_data_path+"_"+str(trainer.current_epoch)+"_"+str(i)+".png")
+         #calculate fid score
+         trainer.logger(experiment=trainer.logger.experiment).log({"fid_score": calculate_fid(ori_data_path, gene_data_path)})
+         #remove generated images
+         os.system("rm -rf /export/home/ra35tiy/vae-inn-synthesis-prober/results/sample_per_epoch/*")
     def on_train_end(self, trainer, pl_module):
         print("Training is ending")
 
 
 # trainer = pl.Trainer(max_epochs=10, devices=devices,logger=logger,callbacks=[checkpoint_callback])  # Set devices=1 for GPU training
-trainer = pl.Trainer(max_epochs=10000, logger=logger, callbacks=[ModelSaveCallback()],accelerator="gpu", devices=4)  # Set devices=1 for GPU training
+trainer = pl.Trainer(max_epochs=400, logger=logger, callbacks=[ModelSaveCallback()],accelerator="gpu", devices=4)  # Set devices=1 for GPU training
 
 # Start training
 trainer.fit(model, train_dataloaders=train_loader)
 
 # Save the model for later inference.
-torch.save(model.state_dict(), "model_cifar10_vae.pt")
+torch.save(model.state_dict(), "model_ffhq.pt")
